0e04160913c6ef050b7488b053764f1ea30e754c
urmilshroff <urmilshroff@gmail.com>
2018-06-17 01:18:11 +0530

    Updated

--- hangman.py
+++ hangman.py
@@ -2,4 +2,4 @@ import random
 
 cars=("Ferrari","Lamborghini","Bavarian Motor Works","Mercedes Benz","Bugatti","Koenigsegg","Pagani","Ford","Chrysler","Chevrolet") #tuple of hangman words
 
-print(cars[random.randint(0,(len(cars)-1))]) #prints a random car from the tuple
+word=cars[random.randint(0,(len(cars)-1))] #picks a random word from tuple as the question
