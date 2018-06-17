d3867401245e9e5bdf8d156cbb5255d2492e779a
urmilshroff <urmilshroff@gmail.com>
2018-06-18 01:34:57 +0530

    Updated

--- hangman.py
+++ hangman.py
@@ -1,17 +1,13 @@
 import numpy
 import random
 
+print("WELCOME TO HANGMAN!")
+
 cars=("ferrari","lamborghini","bavarian motor works","mercedes benz","bugatti","koenigsegg","pagani","ford","chrysler","chevrolet") #tuple of hangman words
 
 question=cars[random.randint(0,(len(cars)-1))] #picks a random word from tuple as the question
-size=len(question)
-answer=numpy.empty(size,str)
-vowels="aeiou"
-
-
 
 def start():
-    print("WELCOME TO HANGMAN!\n")
 
     '''while True:
         level=int(input("Choose your level of difficulty:\n1. Baby\n2. Easy\n3. Medium\n4. Hard\n5. Impossible\n"))
@@ -19,30 +15,31 @@ def start():
             break
         else:
             print("Please enter a valid difficulty level!")'''
+        
+    yoyoyo(question,"aeiou")
 
-def wtf(question,string,answer):
+def yoyoyo(question,string):
+    answer=numpy.empty(len(question),str)
     temp=[]
     
     for i in range(len(question)):
+        
         for j in range(len(string)):
             if (question[i]==string[j]):
                 answer[i]=string[j]
                 temp.append(i) #stores position of matched variables
-    print("Matched positions are",temp)   
     
-    for i in range(len(question)):
         if i not in temp:
             answer[i]="_"
     
     print(question,answer)
     
 
-wtf(question,vowels,answer)
-
-# for letter in range(len(answer)): #question and answer have same length
-#     if (answer[letter]=="_"):
-#         print("It happened in position {}".format(letter))
-
+while True:
+    if input("Do you want to play?\n")!="no":
+        start()
+    else:
+        break
 
 
 
